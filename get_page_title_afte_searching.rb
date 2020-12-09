f8b65ded44a6b33d1afe8995347edc442fa36ccb
Mai Nga <62637072+MaiNgaQT@users.noreply.github.com>
2020-12-09 11:01:50 +0700

    add file search in google

---
+++ get_page_title_afte_searching.rb
@@ -0,0 +1,21 @@
+require 'selenium-webdriver'
+#require 'webdrivers'
+
+driver = Selenium::WebDriver.for :firefox
+
+#open google
+driver.get 'https://www.google.com/'
+
+#maximize browser window
+driver.manage.window.maximize
+
+#search with 'selenium' word
+element = driver.find_element(css: '[name="q"]')
+element.send_keys "selenium"
+element.submit
+sleep 5
+#get page tile and print it
+puts "Page tile is #{driver.title}"
+
+#close browser
+driver.quit
